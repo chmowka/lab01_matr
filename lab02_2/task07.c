0524afacfe8c9f72b294e50760ca714d561307a8
chmowka <154092904+chmowka@users.noreply.github.com>
2025-10-03 12:43:06 +0300

    task07.c

---
+++ lab02_2/task07.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    double x, y, d;
+    int o, p;
+    
+    printf("Введите координаты попадания x y ");
+    scanf("%lf %lf", &x, &y);
+    
+    d = x*x + y*y;
+    
+    if (d <= 4) { 
+        p = 1;  
+    } else if (d <= 16) { 
+        p= 2;  
+    } else {
+        p = 3;  
+    }
+    
+    // Используем switch для определения очков
+    switch (p) {
+        case 1:
+            o = 10;
+            break;
+        case 2:
+            o = 5;
+            break;
+        case 3:
+           o = 0;
+            break;
+        default:
+            o = 0; 
+            break;
+    }
+    
+    printf("координаты (%.2f, %.2f)\n", x, y);
+    printf("попадание  %.2f\n", d);
+    printf("очков %d\n", o );
+    
+    return 0;
+}
