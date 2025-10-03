c06e28e48f6a1b4ae545d4ea32ff45c65ee27e9c
chmowka <154092904+chmowka@users.noreply.github.com>
2025-10-03 10:52:58 +0300

    task05_a.c

--- lab02_2/task05_a.c
+++ lab02_2/task05_a.c
@@ -4,15 +4,24 @@
 int main(){
     int tort_r, tort_z, box_a, box_b, box_c;
     printf("Введите радиус и высоту торта =   \n");
-    printf("a = "); scanf("%ld", &tort_r);
-    printf("a = "); scanf("%ld", &tort_z);
+    printf("радиус торта= "); scanf("%ld", &tort_r);
+    printf("высота торта= "); scanf("%ld", &tort_z);
 
     printf("Введите стороны коробки торта =   \n");
-    printf("a = "); scanf("%ld", box_a);
-    printf("a = "); scanf("%ld", box_b);
-    printf("a = "); scanf("%ld", box_c);
+    printf("введите 1 сторону коробки= "); scanf("%ld", box_a);
+    printf("введите 2 сторону коробки= "); scanf("%ld", box_b);
+    printf("введите высоту короьки  = "); scanf("%ld", box_c);
+
+    if ((tort_r*2)+3 == (box_a*box_b) && (tort_z+3 == box_c)) {
+            print("Торт уместится в коробку");
+        } else{
+           print("Торт  НЕ уместится в коробку");
+        }
+        
+    }
 
     
 
     
+
 }
