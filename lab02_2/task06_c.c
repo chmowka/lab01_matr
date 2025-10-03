84966ca20824afd2990bebfb008dcd138b95901d
chmowka <154092904+chmowka@users.noreply.github.com>
2025-10-03 12:12:48 +0300

    task07.c

---
+++ lab02_2/task06_c.c
@@ -0,0 +1,36 @@
+
+int main() {
+    double num1, num2;
+    int r;
+    char x;
+    
+    printf("Введите первое число: ");
+    scanf("%lf", &num1);
+  
+    printf("Введите второе число: ");
+    scanf("%lf", &num2);
+  
+    printf("Введите операцию +, -, *, / ");
+    scanf(" %c", &x);
+    
+    switch (x) {
+        case '+':  r = num1 + num2; break;
+        case '-':  r = num1 - num2; break;
+        case '*':  r = num1 * num2; break;
+        case '/': 
+            if (num2 != 0) {
+                r = num1 / num2;
+            } else {
+                printf("Ошибка: деление на ноль!\n");
+            }
+            break;
+        default:
+            printf("Ошибка: неизвестная операция!\n");
+            break;
+    }
+    
+
+        printf("%.2f %c %.2f = %.2f\n", num1, x, num2, r);
+    
+    return 0;
+}
