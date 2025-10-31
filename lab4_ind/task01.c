74847a0692dd2861f30d79ef3bbfc949f5cf75de
chmowka <154092904+chmowka@users.noreply.github.com>
2025-10-31 15:21:29 +0300

    Update task01.c

--- lab4_ind/task01.c
+++ lab4_ind/task01.c
@@ -1,31 +1,39 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int main() {
     int num[15];
     
-    // Ввод массива с клавиатуры
     printf("num [0; 90] =  \n");
     for(int i = 0; i < 15; i +=1) {
-        printf("num[%d] = ", i);
-        scanf("%d", &num[i]);
+        // printf("num[%d] = ", i);
+        // scanf("%d", &num[i]);
+        // if(num[i] <= 0 ||  90 <= num[i]){        
+        //     printf("num ne vhodit v diapazon \n \n");
+        //     printf("num[%d] = ", i);
+        //     scanf("%d", &num[i]);
+        // }
+        num[i] = 0+rand() %91;
+        printf("num[%d] =  %d \n", i,num[i]);
     }
     
     
-    // a) Вывод всего массива
     printf("a) all num = ");
     for(int i = 0; i < 15; i+=1) {
         printf("%d ", num[i]);
     }
     
+    printf("\n");
     
-    // b) Элементы с индексами, кратными 2 (четные индексы)
     printf("CHETNIE \n");
     for(int i = 0; i < 15; i += 2) {
         printf("num[%d] = %d\n", i, num[i]);
     }
     printf("\n");
     
-    // c) num > 50
+
+
+
     int num50 = 0;
     for(int i = 0; i < 15; i+=1) {
         if(num[i] > 50) {
@@ -34,7 +42,10 @@ int main() {
     }
     printf("c) kol-Vo  num > 50=  %d \n", num50);
     
-    // d) Sum kratno 3
+
+
+
+
     int sumk3 = 0;
     for(int i = 0; i < 15; i+=1) {
         if(num[i] % 3 == 0) {
@@ -43,9 +54,11 @@ int main() {
     }
     printf("d) Summa num kratni3: %d\n", sumk3);
     
-    // e) Min num pru num >  50
-    int min50 = 100; // начальное большое значение
-    int mini50 = -1;
+
+
+
+    int min50 = 100; 
+    int mini50 = 0;
     
     for(int i = 0; i < 15; i++) {
         if(num[i] > 50) {
@@ -56,17 +69,21 @@ int main() {
         }
     }
     
-    if(mini50 != -1) {
+    if(mini50 != 0) {
         printf("e) min 50 num[%d] = %d\n\n", mini50, min50);
     } else {
         printf("e) not num > 50\n");
     }
     
-    // f) new array  I cdvig  na 2 index
+
+
+
+
+
     int newnum[15];
     
     for(int i = 0; i < 15; i+=1) {
-        newnum[(i + 2) % 15] = num[i];
+        newnum[(i + 2)%15] = num[i];
     }
     
     printf("f) num posle cdviga: ");
