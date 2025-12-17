533c15456184f54a1d4f30c98456176bcdf8f221
chmowka <154092904+chmowka@users.noreply.github.com>
2025-12-17 13:31:40 +0300

    Create l9_task02.c

---
+++ laba9/l9_task02.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <math.h>
+
+void input_m(int m, int n, int mat[m][n]) {
+    printf("VVOD matrici %d x %d\n", m, n);
+    for (int i = 0; i < m; i+=1) {
+        for (int j = 0; j < n; j+=1) {
+            printf("mat[%d][%d] = ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+}
+
+
+void print_m(int m, int n, int mat[m][n]) {
+    printf("Matrica %dx%d:\n", m, n);
+    for (int i = 0; i < m; i+=1) {
+        for (int j = 0; j < n; j+=1) {
+            printf("%d ", mat[i][j]);  
+        }
+        printf("\n"); 
+    }
+}
+
+
+void square_m(int m, int n, int mat[m][n], int result[m][n]) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            result[i][j] = mat[i][j] * mat[i][j]; 
+        }
+    }
+}
+
+int main() {
+    int m, n;
+    
+    printf("KOL-vo strok =");
+    scanf("%d", &m);
+    
+    printf("KOL-vo stolbcov = ");
+    scanf("%d", &n);
+    
+    int matrica[m][n];
+    int squar_mat[m][n];  
+    
+    input_m(m, n, matrica);
+    printf("\n");
+    
+    print_m(m, n, matrica);
+    printf("\n");
+    
+    square_m(m, n, matrica, squar_mat);
+    
+    printf("Matrica s kvadratami \n");
+    print_m(m, n, squar_mat);
+    
+    return 0;
+}
